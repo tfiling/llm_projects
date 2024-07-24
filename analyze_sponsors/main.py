2cd7bec9dac5f89bb9c6724e2a173c8b06d6b29e
galt <galtfilin@gmail.com>
2024-07-24 21:10:21 +0300

    Read csv

--- analyze_sponsors/main.py
+++ analyze_sponsors/main.py
@@ -1,4 +1,17 @@
+import csv
+import pathlib
 
+
+def read_csv_to_dict(path: pathlib.Path) -> list:
+    result = []
+    with open(path, 'r', newline='') as csvfile:
+        csv_reader = csv.DictReader(csvfile)
+        for row in csv_reader:
+            result.append(row)
+    return result
 
 
 if __name__ == '__main__':
+    file_path = pathlib.Path(".") / "data" / "approved_employers.csv"
+    data = read_csv_to_dict(file_path)
+    print(data)
