f007842e3dfc57a4490beb0fa6faa030d6db23fa
galt <galtfilin@gmail.com>
2024-07-31 15:52:34 +0300

    manual_lookup_list.py

---
+++ analyze_sponsors/log_analysis/manual_lookup_list.py
@@ -0,0 +1,12 @@
+websites = {
+    'https://canonical.com/careers'
+    'https://capital.com/careers'
+    'https://emarsys.com/careers/'
+    'https://www.alliancebernstein.com/corporate/en/careers.html'
+    'https://www.avantiwestcoast.co.uk/about-us/careers'
+    'https://www.bigbustours.com/en/london/careers?___store=london_en'
+    'https://www.bloomandwild.com/careers'
+    'https://www.cheniere.com/careers'
+    'https://www.conservation.org/about/conservation-international-jobs'
+    'https://www.flexport.com/careers/jobs/'
+}
